4662f2264f0f61e398834b07ae4c66ade6458283
fluffy-cakes <32635002+fluffy-cakes@users.noreply.github.com>
2021-02-27 14:47:54 +0000

    change to scan only scan directory

--- assets/bin/scanner.sh
+++ assets/bin/scanner.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-files=$(shopt -s nullglob dotglob; echo /data/scan/*)
+SCANDIRECTORY="/data/scan"
+files=$(shopt -s nullglob dotglob; echo "${SCANDIRECTORY}/*")
 
 if (( ${#files} )); then
     printf "Found files to process\n"
 
     for file in "/data/scan"/* ; do
         filename=$(basename "$file")
-        printf "Processing /data/scan/%s\n" "${filename}"
+        printf "Processing %s/%s\n" "${SCANDIRECTORY}" "${filename}"
         printf "[%s]\n" "$(date +'%Y-%m-%d %T')"
 
         printf "Checking and removing completed torrents\n"
@@ -19,12 +20,12 @@ if (( ${#files} )); then
         done
 
         printf "Running scan on completed torrents"
-        clamscan -rio --remove=yes > /data/scan/info 2>&1
+        clamscan -rio --remove=yes ${SCANDIRECTORY} > ${SCANDIRECTORY}/info 2>&1
 
-        if [ -e "/data/scan/${filename}" ]; then
+        if [ -e "${SCANDIRECTORY}/${filename}" ]; then
             printf "  --> File ok, moving\n"
-            mv -f "/data/scan/${filename}" "/data/done/${filename}"
-            rm /data/scan/info
+            mv -f "${SCANDIRECTORY}/${filename}" "/data/done/${filename}"
+            rm ${SCANDIRECTORY}/info
         fi
     done
     printf "Done with processing\n"
