f87905fad93a9778c5801826e7f2331b1a69c2b2
fluffy-cakes <32635002+fluffy-cakes@users.noreply.github.com>
2021-02-27 14:01:35 +0000

    Update install_antivirus.sh

--- assets/install_antivirus.sh
+++ assets/install_antivirus.sh
@@ -2,7 +2,6 @@
 
 # setup cron to update virus signatures hourly
 cd /usr/local || exit
-crontab -l > tempcrons
-echo "5 * * * * /usr/bin/freshclam >> /var/log/cron/general.log 2>&1" >> tempcrons
+echo "0 * * * * /usr/bin/freshclam >> /var/log/cron/general.log 2>&1" >> tempcrons
 crontab tempcrons
 rm tempcrons
